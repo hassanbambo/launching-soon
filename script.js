c3e0cccfe60e82f3cca0a5dc3744326d88d258fe
hassanbambo <114701311+hassanbambo@users.noreply.github.com>
2024-04-26 22:47:13 +0100

    Update script.js
    Properly formatted the timer

--- script.js
+++ script.js
@@ -8,11 +8,18 @@ let x = setInterval( function() {
     let hours = Math.floor((timeGap % (1000*60*60*24)) / (1000*60*60));
     let minutes = Math.floor((timeGap % (1000*60*60)) / (1000*60));
     let seconds = Math.floor((timeGap % (1000*60)) / (1000));
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+	
+	document.getElementById("days").innerHTML = days;
+    days < 10 ? document.getElementById("days").innerHTML = "0" + days : false;
+	
+	document.getElementById("hours").innerHTML = hours;
+    hours < 10 ? document.getElementById("hours").innerHTML = "0" + hours : false;
+	
+	document.getElementById("minutes").innerHTML = minutes;
+    minutes < 10 ? document.getElementById("minutes").innerHTML = "0" + minutes : false;
+	
+	document.getElementById("seconds").innerHTML = seconds;
+    seconds < 10 ? document.getElementById("seconds").innerHTML = "0" + seconds : false;
 
     if (timeGap <= 0) {
         clearInterval(x);
